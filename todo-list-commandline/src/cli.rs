5f4f2934040ddb988f67f788a5bcff02d4740753
habvi <a2231250601h@yahoo.co.jp>
2022-05-22 16:12:36 +0900

    derived from structopt

--- todo-list-commandline/src/cli.rs
+++ todo-list-commandline/src/cli.rs
@@ -1,12 +1,33 @@
 use std::path::PathBuf;
+use structopt::StructOpt;
 
+#[derive(Debug, StructOpt)]
 pub enum Action {
-    Add { text: String },
-    Done { position: usize },
+    /// Write tasks to the journal_file.
+    Add {
+        /// The task description text.
+        #[structopt()]
+        text: String
+    },
+    /// Remove the journal_file by position.
+    Done {
+        #[structopt()]
+        position: usize
+    },
+    /// List all tasks in the journal_file.
     List,
 }
 
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Todo List",
+    about = "A command line to-do app written in Rust"
+)]
 pub struct CommandLineArgs {
+    #[structopt(subcommand)]
     pub action: Action,
+
+    // Use a different journal_file.
+    #[structopt(parse(from_os_str), short, long)]
     pub journal_file: Option<PathBuf>,
 }
--- todo-list-commandline/src/main.rs
+++ todo-list-commandline/src/main.rs
@@ -2,5 +2,11 @@ mod cli;
 use structopt::StructOpt;
 
 fn main() {
+    // cargo run -> help message!
+    cli::CommandLineArgs::from_args();
 
+    // cargo run -- add "task"
+    // cargo run -- done 4
+    // cargo run -- -j xxx.txt list
+    println!("{:#?}", cli::CommandLineArgs::from_args());
 }
