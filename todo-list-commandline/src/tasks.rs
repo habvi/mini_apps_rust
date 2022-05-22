b1939098490a293e7524a96d8183c21d60e29628
habvi <a2231250601h@yahoo.co.jp>
2022-05-22 17:41:02 +0900

    add: add_task func

--- todo-list-commandline/src/tasks.rs
+++ todo-list-commandline/src/tasks.rs
@@ -1,8 +1,9 @@
 use chrono::{serde::ts_seconds, DateTime, Local};
 use serde::Deserialize;
 use serde::Serialize;
-use std::io::Result;
+use std::fs::{File, OpenOptions};
 use std::path::PathBuf;
+use std::io::{Result, Seek, SeekFrom};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -20,7 +21,28 @@ impl Task {
 }
 
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    // Open the file.
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
 
+    // Consume the file's contents as a Vec of tasks.
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    // Rewind the file after reading from it.
+    file.seek(SeekFrom::Start(0))?;
+
+    // Write the modified task list back into the file.
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
 }
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
